f3162f40843a35a9e8d248a6357d27a86dad0539
Jamie Klein <jamiesklein@gmail.com>
2017-12-01 16:26:11 +0000

    Adds pedning tests to collector test.

---
+++ specs/collector_spec.js
@@ -0,0 +1,16 @@
+let assert = require('assert');
+let RecordStore = require('../source/record_store');
+let Record = require('../source/record');
+
+describe('Record Collector', function() {
+  it('should have cash');
+  it('should be able to buy records');
+  it('shouldn\'t be able to buy records they can\'t afford');
+  it('should be able to sell records');
+  it('should be able to get total records value');
+  it('should be able to get total records value of a genre');
+  it('should be able to their most valuable record');
+  it('should be able to sort their records ascending');
+  it('should be able to sort their records descending');
+  it('should be able to compare the value of their record collection');
+});
