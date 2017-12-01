25e69a76ea7b5f5aa8218ab9b1a73efcd30720a9
Jamie Klein <jamiesklein@gmail.com>
2017-12-01 14:44:38 +0000

    Adds beforeEach to Record test and constructor for Record.

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -2,6 +2,12 @@ let assert = require('assert');
 let Record = require('../source/record');
 
 describe('Record', function() {
+  let record;
+
+  beforeEach(function() {
+    record = new Record('Alex Cameron', 'Jumping the Shark', 'Alt-Rock', 15);
+  });
+
   it('should have an artist');
   it('should have a title');
   it('should have a genre');
