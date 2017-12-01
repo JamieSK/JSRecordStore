b211e3000eb9736d53e117447bc1d540e5e3dbbb
Jamie Klein <jamiesklein@gmail.com>
2017-12-01 14:40:49 +0000

    Adds pending tests for Record.

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -2,5 +2,8 @@ let assert = require('assert');
 let Record = require('../source/record');
 
 describe('Record', function() {
-
+  it('should have an artist');
+  it('should have a title');
+  it('should have a genre');
+  it('should have an price');
 });
